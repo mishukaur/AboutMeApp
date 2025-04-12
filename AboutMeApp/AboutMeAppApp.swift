67e8431aa4efc3e85826477568d00eb1e6f1e812
Manjot Kaur <manjotk2003@gmail.com>
2025-04-12 14:25:10 -0400

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Manjot Kaur on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
